87cad9a7b1dc490fb98bfe125180d0d9dfe8c540
LsFbr <louisfabre@hotmail.fr>
2026-04-17 10:23:07 +0200

    feat(profiles/migrations): add migration to copy profiles from oc_lettings_site

---
+++ profiles/migrations/0002_auto_20260417_1006.py
@@ -0,0 +1,27 @@
+from django.db import migrations
+
+
+def copy_profiles(apps, schema_editor):
+    OldProfile = apps.get_model("oc_lettings_site", "Profile")
+    NewProfile = apps.get_model("profiles", "Profile")
+
+    for old in OldProfile.objects.all():
+        
+        if not NewProfile.objects.filter(user_id=old.user_id).exists():
+            NewProfile.objects.create(
+                id=old.id,
+                user_id=old.user_id,
+                favorite_city=old.favorite_city,
+            )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("profiles", "0001_initial"),
+        ("oc_lettings_site", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(copy_profiles, migrations.RunPython.noop),
+    ]
