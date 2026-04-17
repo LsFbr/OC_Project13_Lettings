0228f573186320a706f1451e43d1d8eec14badfd
LsFbr <louisfabre@hotmail.fr>
2026-04-17 10:05:01 +0200

    feat(lettings/migrations): add migration to copy Address and Letting data from oc_lettings_site

---
+++ lettings/migrations/0002_auto_20260417_0955.py
@@ -0,0 +1,43 @@
+from django.db import migrations
+
+
+def copy_addresses(apps, schema_editor):
+    OldAddress = apps.get_model("oc_lettings_site", "Address")
+    NewAddress = apps.get_model("lettings", "Address")
+
+    for old in OldAddress.objects.all():
+        NewAddress.objects.create(
+            id=old.id,
+            number=old.number,
+            street=old.street,
+            city=old.city,
+            state=old.state,
+            zip_code=old.zip_code,
+            country_iso_code=old.country_iso_code,
+        )
+
+
+def copy_lettings(apps, schema_editor):
+    OldLetting = apps.get_model("oc_lettings_site", "Letting")
+    NewLetting = apps.get_model("lettings", "Letting")
+
+    for old in OldLetting.objects.all():
+        NewLetting.objects.create(
+            id=old.id,
+            title=old.title,
+            address_id=old.address_id,
+        )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("lettings", "0001_initial"),
+        ("oc_lettings_site", "0001_initial"),
+    ]
+
+    # Copy addresses first to avoid foreign key breaks
+    operations = [
+        migrations.RunPython(copy_addresses),
+        migrations.RunPython(copy_lettings),
+    ]
