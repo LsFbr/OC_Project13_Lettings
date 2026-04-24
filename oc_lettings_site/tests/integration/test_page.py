4b36ff4c513a81736699dea69f69d7c592507b1a
LsFbr <louisfabre@hotmail.fr>
2026-04-24 16:00:15 +0200

    test(oc_lettings_site/tests/integration/test_page.py): add integration tests for index, 404, and 500 error views in the oc_lettings_site app

---
+++ oc_lettings_site/tests/integration/test_page.py
@@ -0,0 +1,43 @@
+"""Integration tests for views in the oc_lettings_site app."""
+
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
+
+
+# //Index view tests
+def test_index_view(client):
+    """Ensure the index view returns HTTP 200."""
+
+    response = client.get(reverse("index"))
+    content = response.content.decode()
+    expected_content = "Welcome to Holiday Homes"
+
+    assert expected_content in content
+    assert response.status_code == 200
+    assertTemplateUsed(response, "index.html")
+
+
+def test_500_error_view(client):
+    # disable Django's default exception handling to test the status code
+    client.raise_request_exception = False
+
+    response = client.get(reverse("500-error"))
+    content = response.content.decode()
+    expected_content = "Server error"
+
+    assert expected_content in content
+    assert response.status_code == 500
+    assertTemplateUsed(response, "500.html")
+
+
+# //404 error view tests
+def test_404_page(client):
+    """Ensure the 404 error is returned when a non-existent URL is accessed."""
+
+    response = client.get("/url-inexistante/")
+    content = response.content.decode()
+    expected_content = "Page not found"
+
+    assert expected_content in content
+    assert response.status_code == 404
+    assertTemplateUsed(response, "404.html")
